90d9a171776938168d4c8504bcda225cc0c70599
Weiner Oliveira <woliveira82@gmail.com>
2018-10-05 20:55:36 -0300

    Async/await corrections. Count variable deleted. Font modification

--- js/index.js
+++ js/index.js
@@ -1,31 +1,41 @@
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", async _ => {
     let term = document.getElementById('term');
-    term.innerHTML = "|";
-
     let response = await getDomains();
-    let count = 0;
-    response.domains.forEach((domainObject, i) => {
+
+    for (domainObject of response.domains) {
         let letters = domainObject.domain.split('');
+
         for (let letter of letters) {
-            timeout(() => { term.innerHTML = term.innerHTML.substring(0, term.innerHTML.length - 1) + letter + '|' }, 110 * count);
-            count++;
+            await timeout(_ => {
+                term.innerHTML = term.innerHTML.substring(0, term.innerHTML.length - 2) + letter + ' _';
+            }, 100);
         }
-        count += 5;
+
+        await sleep(1000);
+
         for (let i = letters.length; i > 0; i--) {
-            timeout(() => { term.innerHTML = term.innerHTML.substring(0, i - 1) + '|' }, 110 * count);
-            count++;
+            await timeout(_ => {
+                term.innerHTML = term.innerHTML.substring(0, i - 2) + ' _';
+            }, 50);
         }
-    });
+    }
 });
 
-function timeout(callback, ms) {
+let timeout = async function timeout(callback, ms) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => { resolve(callback()) }, ms);
+        setTimeout(_ => {
+            resolve(callback());
+        }, ms);
     });
 }
 
-async function getDomains() {
-    let response = await fetch('http://localhost:3030');
+let getDomains = async function getDomains() {
+    let response = await fetch("http://localhost:3030");
+    //let response = await fetch('https://api.domainsdb.info/search?query=google&tld=com');
     let json = await response.json();
     return json;
+}
+
+let sleep = async function (ms) {
+    return timeout(_ => { }, ms);
 }
